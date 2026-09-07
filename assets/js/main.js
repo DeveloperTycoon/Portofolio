3d3f256a910901683c6b072f407161576217ad93
NML <bloopingguys13@gmail.com>
2026-09-07 21:27:49 +0800

    Major Update
    - Icon new!
    - Star update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,28 @@
+// NML Animation
+
 document.addEventListener("DOMContentLoaded", () => {
   
   
   const tl = gsap.timeline({ repeat: -1 });
 
   tl.to(".wavy-text span", {
-    y: -16,               // Ketinggian puncak ombak
-    scaleY: 1.05,         // Sedikit meregang seperti air naik
-    duration: 2,        // Kecepatan alir ombak
-    ease: "sine.inOut",   // Kurva halus khas fluida/air
+    y: -16,               
+    scaleY: 1.05,         
+    duration: 2,        
+    ease: "sine.inOut",   
     stagger: {
-      each: 0.50,         // Jeda waktu aliran dari N -> M -> L
+      each: 0.50,         
       from: "start",
-      repeat: -1,         // Loop kontinyu untuk tiap huruf
+      repeat: -1,        
       yoyo: true
     }
   });
 
 });
+
+
+
+// Cursor Glow
 
 const glow = document.getElementById('glow');
 
@@ -27,6 +33,10 @@ window.addEventListener('pointermove', (e) => {
   glow.style.transform= `translate(${x - 100}px, ${y - 100}px)`;
 
 })
+
+
+
+// P Writer
 
 const textElement = document.getElementById("TextSec");
 const words = ["Let's create something that lasts forever!","It's time we make something unforgettable!","A developer that learn by doing!"]
@@ -64,3 +74,51 @@ function typeEffect() {
 
 document.addEventListener("DOMContentLoaded", typeEffect)
 
+
+// Star Effect
+
+const jumlahBintang = 300; 
+
+    for (let i = 0; i < jumlahBintang; i++) {
+      const star = document.createElement('div');
+      star.classList.add('star');
+      const fullWidth = Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth
+      );
+      const fullHeight = Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight
+      );
+      
+      const x = Math.random() * fullWidth;
+      const y = Math.random() * fullHeight;
+
+      
+      const size = Math.random() * 2 + 1;
+
+      
+      const duration = Math.random() * 2 + 1; 
+      const delay = Math.random() * 3; 
+
+      star.style.left = `${x}px`;
+      star.style.top = `${y}px`;
+      star.style.width = `${size}px`;
+      star.style.height = `${size}px`;
+      star.style.setProperty('--duration', `${duration}s`);
+      star.style.animationDelay = `${delay}s`;
+
+      document.body.appendChild(star);
+}
+
+
+// Audio
+
+const audio = document.getElementById('myAudio');
+
+audio.volume = 0.1
+
+
+// Testing begin
+
+// Tesing end
