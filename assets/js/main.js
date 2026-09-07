8d0906deb74db6594b3012e359c2fd519686ed7b
AtuoWeb <bloopingguys13@gmail.com>
2026-09-07 09:56:37 +0800

    Update UI
    More Interactive!!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,40 @@ window.addEventListener('pointermove', (e) => {
   glow.style.transform= `translate(${x - 100}px, ${y - 100}px)`;
 
 })
+
+const textElement = document.getElementById("TextSec");
+const words = ["Let's create something that lasts forever!","It's time we make something unforgettable!","A developer that learn by doing!"]
+
+
+let wordsIndex = 0;
+let charIndex = 0;
+let IsDeleting = false;
+
+function typeEffect() {
+  const currentWord = words[wordsIndex]
+  const currentText = currentWord.substring(0, charIndex);
+
+  textElement.innerHTML = `${currentText}<span class="cursor">&nbsp;</span>`;
+
+  if (IsDeleting) {
+    charIndex--;
+  } else {
+    charIndex++;
+  }
+
+  let typeSpeed = IsDeleting ? 20 : 60;
+
+  if (!IsDeleting && charIndex > currentWord.length) {
+    typeSpeed = 2000;
+    IsDeleting = true;
+  } else if (IsDeleting && charIndex < 0) {
+    IsDeleting = false;
+    wordsIndex = (wordsIndex + 1) % words.length;
+    typeSpeed = 500;
+  }
+
+  setTimeout(typeEffect, typeSpeed)
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect)
+
